1294b6fdfe8641156c6c5965f25f990206d0f372
Julien Castro <julien.castro@protonmail.com>
2024-05-29 11:27:58 +0200

    premier commit portefolio

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#FD853A',
+        'secondary': '#FFFFFF',
+        'tertariy': '#000000',
+      }
+    },
+  },
+  plugins: [
+
+  ],
+}
+
