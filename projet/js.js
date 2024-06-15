862c3d558ed8b9c30a02d31eaa6fc5be326b313a
Julien Castro <julien.castro@protonmail.com>
2024-06-15 16:48:18 +0200

    ajout de petit onion

--- projet/js.js
+++ projet/js.js
@@ -13,7 +13,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 "API": '<img src="https://cdn-icons-png.flaticon.com/512/2906/2906260.png" alt="API Icon" class="icon-img" width="25" height="25" />',
                 "TailwindCSS": '<img src="https://cdn-icons-png.flaticon.com/512/732/732190.png" alt="TailwindCSS Icon" class="icon-img" width="25" height="25" />',
                 "Telegram": '<img src="https://cdn-icons-png.flaticon.com/512/2111/2111646.png" alt="Telegram Icon" class="icon-img" width="25" height="25" />',
-                "WordPress": '<img src="https://cdn-icons-png.flaticon.com/512/174/174881.png" alt="WordPress Icon" class="icon-img" width="25" height="25" />'
+                "WordPress": '<img src="https://cdn-icons-png.flaticon.com/512/174/174881.png" alt="WordPress Icon" class="icon-img" width="25" height="25" />',
+                "Laravel": '<img src="/assets/icon/laravel.svg" alt="WordPress Icon" class="icon-img" width="25" height="25" />',
             };
 
             const projectsGrid = document.getElementById('projects-grid');
@@ -22,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 const projectCard = document.createElement('div');
                 projectCard.classList.add('project-card', 'text-center', 'bg-white', 'rounded-full', 'shadow-md', 'overflow-hidden', 'p-4', 'm-2');
                 projectCard.innerHTML = `
-                    <img src="${project.image}" alt="${project.title}" class="rounded-full mx-auto w-full h-48 object-contain">
+                    <img src="${project.image}" alt="${project.title}" class="rounded-xl mx-auto w-full h-48 object-contain">
                     <h3 class="mt-4 text-lg font-semibold text-black">${project.title}</h3>
                     <p class="mt-2 text-sm text-zinc-600 dark:text-zinc-400">${project.description}</p>
                     <div class="flex flex-wrap justify-center gap-2 mt-2">
