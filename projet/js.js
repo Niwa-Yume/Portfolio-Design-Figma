69cebbc83e97824be8ab680a0b5abfa9160aa727
Julien Castro <julien.castro@protonmail.com>
2024-06-17 15:00:59 +0200

    fix chemin

--- projet/js.js
+++ projet/js.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('/projet.json') // Utilisation d'un chemin absolu
+    fetch('projet.json') // Utilisation d'un chemin absolu
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);
