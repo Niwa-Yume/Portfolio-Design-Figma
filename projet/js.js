5adc1217142114737f8df5f0b1c247b9e2c2748d
Julien Castro <julien.castro@protonmail.com>
2024-06-22 14:09:07 +0200

    pûblic image

--- projet/js.js
+++ projet/js.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('/projet.json') // Utilisation d'un chemin relatif à partir de la racine
+    fetch('/projet.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);
@@ -7,7 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
             return response.json();
         })
         .then(projects => {
-            console.log('Projets chargés:', projects); // Journalisation pour vérifier le contenu du JSON
+            console.log('Projets chargés:', projects);
+
             const techIcons = {
                 "HTML": '<img src="https://cdn-icons-png.flaticon.com/512/732/732212.png" alt="HTML Icon" class="icon-img" width="25" height="25" />',
                 "CSS": '<img src="https://cdn-icons-png.flaticon.com/512/732/732190.png" alt="CSS Icon" class="icon-img" width="25" height="25" />',
@@ -17,10 +18,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 "Python": '<img src="https://cdn-icons-png.flaticon.com/512/919/919852.png" alt="Python Icon" class="icon-img" width="25" height="25" />',
                 "Django": '<img src="https://cdn-icons-png.flaticon.com/512/919/919831.png" alt="Django Icon" class="icon-img" width="25" height="25" />',
                 "API": '<img src="https://cdn-icons-png.flaticon.com/512/2906/2906260.png" alt="API Icon" class="icon-img" width="25" height="25" />',
-                "TailwindCSS": '<img src="../assets/icon tools/tailwindcss-icon.svg" alt="Tailwind CSS" class="w-8 h-8">',
+                "TailwindCSS": '<img src="/assets/svg/tailwindcss-icon.svg" alt="Tailwind CSS" class="w-8 h-8">',
                 "Telegram": '<img src="https://cdn-icons-png.flaticon.com/512/2111/2111646.png" alt="Telegram Icon" class="icon-img" width="25" height="25" />',
                 "WordPress": '<img src="https://cdn-icons-png.flaticon.com/512/174/174881.png" alt="WordPress Icon" class="icon-img" width="25" height="25" />',
-                "Laravel": '<img src="../assets/icon/laravel.svg" alt="Laravel Icon" class="icon-img" width="25" height="25" />',
+                "Laravel": '<img src="/assets/icon/laravel.svg" alt="Laravel Icon" class="icon-img" width="25" height="25" />',
             };
 
             const projectsGrid = document.getElementById('projects-grid');
@@ -29,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 const projectCard = document.createElement('div');
                 projectCard.classList.add('project-card', 'text-center', 'bg-white', 'rounded-lg', 'shadow-lg', 'overflow-hidden', 'p-6', 'm-4', 'transition-transform', 'transform', 'hover:scale-105');
                 projectCard.innerHTML = `
-                    <img src="..${project.image}" alt="${project.title}" class="rounded-t-lg w-full h-64 object-cover">
+                    <img src="/image projet/${project.image}" alt="${project.title}" class="rounded-t-lg w-full h-64 object-cover">
                     <div class="p-4">
                         <h3 class="mt-2 text-2xl font-semibold text-black">${project.title}</h3>
                         <p class="mt-2 text-base text-zinc-600 dark:text-zinc-400">${project.description}</p>
